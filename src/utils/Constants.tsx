4c1bfd62c075faf49140ffdea47b4c292c2f6cfd
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-08-19 14:23:23 +0530

    fonts linked

--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -8,11 +8,24 @@ export enum Colors {
     backgroundSecondary = '#f5f6fb'
 }
 export enum Fonts {
-    Regular = 'Okra-Regular',
-    Medium = 'Okra-Medium',
-    Light = 'Okra-MediumLight',
-    SemiBold = 'Okra-Bold',
-    Bold = 'Okra-ExtraBold',
+    Poppins_Thin_100 = 'Poppins-Thin',
+    Poppins_Thin_Italic_100 = 'Poppins-ThinItalic',
+    Poppins_ExtraLight_200 = 'Poppins-ExtraLight',
+    Poppins_ExtraLight_Italic_200 = 'Poppins-ExtraLightItalic',
+    Poppins_Light_300 = 'Poppins-Light',
+    Poppins_Light_Italic_300 = 'Poppins-LightItalic',
+    Poppins_Regular_400 = 'Poppins-Regular',
+    Poppins_Regular_Italic_400 = 'Poppins-Italic',
+    Poppins_Medium_500 = 'Poppins-Medium',
+    Poppins_Medium_Italic_500 = 'Poppins-MediumItalic',
+    Poppins_SemiBold_600 = 'Poppins-SemiBold',
+    Poppins_SemiBold_Italic_600 = 'Poppins-SemiBoldItalic',
+    Poppins_Bold_700 = 'Poppins-Bold',
+    Poppins_Bold_Italic_700 = 'Poppins-BoldItalic',
+    Poppins_ExtraBold_800 = 'Poppins-ExtraBold',
+    Poppins_ExtraBold_Italic_800 = 'Poppins-ExtraBoldItalic',
+    Poppins_Black_900 = 'Poppins-Black',
+    Poppins_Black_Italic_900 = 'Poppins-BlackItalic',
 }
 
 export const lightColors = [
