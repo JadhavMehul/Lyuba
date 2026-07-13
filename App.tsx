3b17d47a66a59c415574d6c8206db2d3fd801a25
tirthak <gadgetzone@tirthak-MacBook-Pro.local>
2026-07-14 02:56:46 +0530

    account info

--- App.tsx
+++ App.tsx
@@ -29,6 +29,7 @@ import SettingScreen from '@features/settingss/SettingScreen';
 import EditProfile from '@features/settingss/EditProfile';
 import EditImage from '@features/settingss/EditImage';
 import NotificationScreen from '@features/settingss/NotificationScreen';
+import AccountInfo from '@features/settingss/AccountInfo';
 
 const Stack = createNativeStackNavigator();
 
@@ -114,6 +115,7 @@ const App = () => {
               <Stack.Screen name="EditProfile" component={EditProfile}/>
               <Stack.Screen name="EditImage" component={EditImage}/>
               <Stack.Screen name="NotificationScreen" component={NotificationScreen}/>
+              <Stack.Screen name="AccountInfo" component={AccountInfo}/>
             </>
           )
         }
